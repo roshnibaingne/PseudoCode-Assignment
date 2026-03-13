21a5e7c296f76969297635adb048f0687acfaa86
roshnibaingne <166687972+roshnibaingne@users.noreply.github.com>
2026-03-13 21:53:47 +0530

    Add files via upload

---
+++ peudoCode/PseudoCode-Assignment/assignment48.java
@@ -0,0 +1,15 @@
+// PROCEDURE add(a:INTEGER,b:INTEGER)
+// PRINT a+b
+// END PROCEDURE
+// CALL add(2,3)
+
+public class assignment48{
+    public static void add(int a, int b) {
+        System.out.println(a + b);
+    }
+
+    public static void main(String[] args) {
+        add(2, 3);
+    }
+}
+
